abdb3a7551ef7c27198aa536d731c22a9fe5e048
Ginver <74971813+Ginver@users.noreply.github.com>
2023-09-10 14:47:02 +0200

    checkboxes checked show/hide inputfields added

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+document.getElementById("metricCheck").checked = true;
+
+const metricRadioButton = document.getElementById("metricCheck");
+const imperialRadioButton = document.getElementById("imperialCheck");
+
+const metricDiv = document.querySelector(".input__measurement--metric");
+const imperialDiv = document.querySelector(".input__measurement--imperial");
+
+metricRadioButton.addEventListener("change", function () {
+  if (metricRadioButton.checked) {
+    console.log(metricRadioButton);
+    metricDiv.style.display = "flex";
+    imperialDiv.style.display = "none";
+  }
+});
+
+imperialRadioButton.addEventListener("change", function () {
+  if (imperialRadioButton.checked) {
+    metricDiv.style.display = "none";
+    imperialDiv.style.display = "flex";
+  }
+});
