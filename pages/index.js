ef879a3941a6b8a405ab01f9c7f8eb981fdb05b5
Ginver <74971813+Ginver@users.noreply.github.com>
2023-09-11 12:28:28 +0200

    onload metric option is by default

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,14 @@ const imperialRadioButton = document.getElementById("imperialCheck");
 const metricDiv = document.querySelector(".input__measurement--metric");
 const imperialDiv = document.querySelector(".input__measurement--imperial");
 
+metricRadioButton.addEventListener("onload", function () {
+  if (metricRadioButton.checked) {
+    console.log(metricRadioButton);
+    metricDiv.style.display = "flex";
+    imperialDiv.style.display = "none";
+  }
+});
+
 metricRadioButton.addEventListener("change", function () {
   if (metricRadioButton.checked) {
     console.log(metricRadioButton);
